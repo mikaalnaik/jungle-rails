59b511761838b8299839d19d4130ee4746acfe51
Mikaal Naik <matthewnaik@gmail.com>
2018-05-25 16:17:17 +0000

    email recipt

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ApplicationMailer
 
   default from: 'notifications@example.com'
 
- def welcome_email(user)
+ def welcome_email(user,order)
+   @order = order
    @user = user
    @url  = 'http://example.com/login'
    mail(to: user.email, subject: 'Welcome to My Awesome Site')
